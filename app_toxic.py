fe964592c001e5d5f6322e4e72ca8f666593c45b
Mohammed Shareef <44450467+mohammedsharee@users.noreply.github.com>
2021-02-27 21:19:04 +0100

    Update app_toxic.py

--- app_toxic.py
+++ app_toxic.py
@@ -18,42 +18,82 @@ import streamlit as st
 # pickle_in=open('classifier.pkl','rb')
 # classifier=pickle.load(pickle_in)
 
-tox_vec=open('toxic_dict.pkl','rb')
-tox = pickle.load(tox_vec)
 
-severe_toxic_dict=open("severe_toxic_dict.pkl", "rb") 
-sev = pickle.load(severe_toxic_dict)
+# tox_vec=open('toxic_dict.pkl','rb')
+# tox = pickle.load(tox_vec)
 
-obscene_dict= open("obscene_dict.pkl", "rb") 
-obs = pickle.load(obscene_dict)
+# severe_toxic_dict=open("severe_toxic_dict.pkl", "rb") 
+# sev = pickle.load(severe_toxic_dict)
 
-insult_dict=open(r"insult_dict.pkl", "rb")
-ins = pickle.load(insult_dict)
+# obscene_dict= open("obscene_dict.pkl", "rb") 
+# obs = pickle.load(obscene_dict)
 
-threat_dict=open(r"threat_dict.pkl", "rb")
-thr = pickle.load(threat_dict)
+# insult_dict=open(r"insult_dict.pkl", "rb")
+# ins = pickle.load(insult_dict)
 
-identity_hate_dict=open("identity_hate_dict.pkl", "rb")
-ide = pickle.load(identity_hate_dict)
+# threat_dict=open(r"threat_dict.pkl", "rb")
+# thr = pickle.load(threat_dict)
+
+# identity_hate_dict=open("identity_hate_dict.pkl", "rb")
+# ide = pickle.load(identity_hate_dict)
+
+# # Load the pickled RDF models
+# toxic_model=open("toxic_model.pkl", "rb")
+# tox_model = pickle.load(toxic_model)
+
+# severe_toxic_model=open("severe_toxic_model.pkl", "rb")
+# sev_model = pickle.load(severe_toxic_model)
+
+# obscene_model=open("obscene_model.pkl", "rb")
+# obs_model  = pickle.load(obscene_model)
+
+# insult_model=open("insult_model.pkl", "rb")
+# ins_model  = pickle.load(insult_model)
+
+# threat_model=open("threat_model.pkl", "rb")
+# thr_model  = pickle.load(threat_model)
+
+# identity_hate_model=open("identity_hate_model.pkl", "rb")
+# ide_model  = pickle.load(identity_hate_model)
+
+with open(r"toxic_dict.pkl", "rb") as f:
+    tox = pickle.load(f)
+
+with open(r"severe_toxic_dict.pkl", "rb") as f:
+    sev = pickle.load(f)
+
+with open(r"obscene_dict.pkl", "rb") as f:
+    obs = pickle.load(f)
+
+with open(r"insult_dict.pkl", "rb") as f:
+    ins = pickle.load(f)
+
+with open(r"threat_dict.pkl", "rb") as f:
+    thr = pickle.load(f)
+
+with open(r"identity_hate_dict.pkl", "rb") as f:
+    ide = pickle.load(f)
 
 # Load the pickled RDF models
-toxic_model=open("toxic_model.pkl", "rb")
-tox_model = pickle.load(toxic_model)
+with open(r"toxic_model.pkl", "rb") as f:
+    tox_model = pickle.load(f)
 
-severe_toxic_model=open("severe_toxic_model.pkl", "rb")
-sev_model = pickle.load(severe_toxic_model)
+with open(r"severe_toxic_model.pkl", "rb") as f:
+    sev_model = pickle.load(f)
 
-obscene_model=open("obscene_model.pkl", "rb")
-obs_model  = pickle.load(obscene_model)
+with open(r"obscene_model.pkl", "rb") as f:
+    obs_model  = pickle.load(f)
 
-insult_model=open("insult_model.pkl", "rb")
-ins_model  = pickle.load(insult_model)
+with open(r"insult_model.pkl", "rb") as f:
+    ins_model  = pickle.load(f)
 
-threat_model=open("threat_model.pkl", "rb")
-thr_model  = pickle.load(threat_model)
+with open(r"threat_model.pkl", "rb") as f:
+    thr_model  = pickle.load(f)
 
-identity_hate_model=open("identity_hate_model.pkl", "rb")
-ide_model  = pickle.load(identity_hate_model)
+with open(r"identity_hate_model.pkl", "rb") as f:
+    ide_model  = pickle.load(f)
+ 
+   
 
 
  
